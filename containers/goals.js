776a99b19415417a8dbf2fc8bc6bc5210bbafb34
arielcook <arielcook101014@gmail.com>
2016-08-01 15:46:42 -0700

    [Add folders] reducers and containers

--- containers/goals.js
+++ containers/goals.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
 import {connect} from 'react-redux';
 
+<<<<<<< HEAD
 var Line = require('rc-progress').Line;
 var SPACE = ".............";
 
@@ -36,3 +37,18 @@ function mapStateToProps(state) {
 }
 
 export default connect(mapStateToProps)(ProgressBar);
+=======
+class ProgressBar extends Component {
+  renderProgressBar() {
+    return (?????)
+  }
+
+
+
+  render() {
+    return (
+      <div className='progress-bar'></div>
+    )
+  }
+}
+>>>>>>> [Add folders] reducers and containers
