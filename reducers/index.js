e8baf0289cfebfcbed509d1731eee261e07459c2
arielcook <arielcook101014@gmail.com>
2016-08-01 15:46:42 -0700

    [Add folders] reducers and containers

--- reducers/index.js
+++
@@ -1,8 +0,0 @@
-import { combineReducers } from 'redux'
-import UserGoals from './reducer_goals'
-
-const rootReducers = combineReducers({
-  userGoals: UserGoals
-})
-
-export default rootReducers;
