df915a0f69f5455a5576e0cc55f3035f26601ee1
Denny Jun <denny.6a756e@gmail.com>
2016-08-02 01:21:24 -0700

    [config] implement hotloading

---
+++ webpack-server.js
@@ -0,0 +1,26 @@
+var config = require('./webpack.config');
+var webpack = require('webpack');
+var WebpackDevServer = require('webpack-dev-server');
+
+module.exports = (function() {
+  var compiler = webpack(config);
+  return function(host, port) {
+    var options = {
+      contentBase: 'http://' + host + ':' + (port + 1),
+      hot: true,
+      inline: true,
+      lazy: false,
+      publicPath: config.output.publicPath,
+      compress: true,
+      quiet: false,
+      noInfo: true,
+      headers: { 'Access-Control-Allow-Origin': '*' }
+    };
+    var webpackDevServer = new WebpackDevServer(compiler, options);
+    webpackDevServer.listen(port + 1, host, function() {
+      console.log("Webpack development server listening on %s:%s", 
+        host, 
+        port + 1);
+    });
+  };
+})();
