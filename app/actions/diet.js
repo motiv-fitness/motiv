d69b8d2c1b26c6f5579a4ea58a68bca6816e05f6
Jason Lee <jasonjlee.5@gmail.com>
2016-08-02 22:24:40 -0700

    [merge]Pulling recent work

--- app/actions/diet.js
+++ app/actions/diet.js
@@ -9,5 +9,25 @@ export function display(name, exercise){
       name:name,
       exercise:exercise
     });
-  }
+    return fetch('/DISPLAY_REGIME',{
+      method:'GET',
+      headers:{'Content-Type':'application/json'},
+    }).then((response)=>{
+      if(response.ok) {
+        return response.json().then((json)=>{
+          dispatch({
+            type:'DISPLAY_REGIME_SUCCESS',
+            name:name,
+            exercise:exercise
+          });
+        });
+      } else {
+        return response.json().then((json) => {
+          dispatch({
+            type: 'DISPLAY_REGIME_FAIL',
+            name:json,
+            exercise:json
+      }
+    });
+  };
 }
