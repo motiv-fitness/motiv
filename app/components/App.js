e9506b655a08980e7a813762d4309cbb694cfa42
arielcook <arielcook101014@gmail.com>
2016-08-03 13:26:23 -0700

    [feature] setup files for progressbar

--- app/components/App.js
+++ app/components/App.js
@@ -2,13 +2,11 @@ import React from 'react';
 import Header from './Header';
 import Footer from './Footer';
 import ProgressBar from '../containers/goals';
-
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 
 //needed for material-ui
 import injectTapEventPlugin from 'react-tap-event-plugin';
 injectTapEventPlugin();
-
 
 class App extends React.Component {
   render() {
