3c2747f71f24c099741bd11abbd8c82b45284b19
arielcook <arielcook101014@gmail.com>
2016-08-03 11:47:05 -0700

    [PROGRESS BAR] set up folders and files

--- containers/goals.js
+++ containers/goals.js
@@ -3,14 +3,24 @@ import {connect} from 'react-redux';
 
 class ProgressBar extends Component {
   renderProgressBar() {
-    return (?????)
+    return this.props.images.map((user) => {
+      return (
+        // AC: START WORK HERE
+      )
+    })
   }
 
-
-
-  render() {
+render() {
     return (
       <div className='progress-bar'></div>
     )
   }
 }
+
+function mapStateToProps(state) {
+  return {
+    users: state.UserGoals
+  };
+}
+
+export default connect(mapStateToProps)(ProgressBar);
