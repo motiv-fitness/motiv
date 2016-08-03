bde25b679458522a160c950406fcd608240890f0
arielcook <arielcook101014@gmail.com>
2016-08-03 13:26:23 -0700

    [feature] setup files for progressbar

--- app/components/App.js
+++ app/components/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import Header from './Header';
 import Footer from './Footer';
+import ProgressBar from '../containers/goals';
 
 class App extends React.Component {
   render() {
     return (
       <div>
         <Header/>
+        <ProgressBar/>
         {this.props.children}
         <Footer/>
       </div>
