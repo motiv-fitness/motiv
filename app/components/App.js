7a74d15665e4392681626b83f4a37c8e6fc401c1
arielcook <arielcook101014@gmail.com>
2016-08-03 13:26:23 -0700

    [feature] setup files for progressbar

--- app/components/App.js
+++ app/components/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Header from './Header';
 import Footer from './Footer';
+import ProgressBar from '../containers/goals';
 
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 
@@ -15,7 +16,8 @@ class App extends React.Component {
       <MuiThemeProvider>
         <div>
           <Header/>
-          {this.props.children}
+          <ProgressBar/>
+            {this.props.children}
           <Footer/>
         </div>
       </MuiThemeProvider>
--- containers/goals.js
+++ containers/goals.js
@@ -1,18 +1,30 @@
 import React, {Component} from 'react';
 import {connect} from 'react-redux';
 
+var Line = require('rc-progress').Line;
+var SPACE = ".............";
+
 class ProgressBar extends Component {
   renderProgressBar() {
     return this.props.images.map((user) => {
       return (
-        // AC: START WORK HERE
+        <div>
+          <h3>Progress Bar</h3>
+          <Line percent={25} />
+          <div>
+            <strong>Name:</strong> {user.name} {SPACE}
+          </div>
+        </div>
       )
     })
   }
 
 render() {
+    console.log("Inside goals.js====================")
     return (
-      <div className='progress-bar'></div>
+      <div>
+        {this.renderProgressBar()}
+      </div>
     )
   }
 }
