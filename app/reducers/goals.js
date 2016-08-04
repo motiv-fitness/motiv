ae7285b954f879219cfa29f63495e3f2835ed42e
Ariel <arielcook101014@gmail.com>
2016-08-04 11:26:40 -0700

    [bug] fix odd git rebase issue

--- app/reducers/goals.js
+++ app/reducers/goals.js
@@ -3,10 +3,6 @@ export default function() {
     { name: "Justin", image: "https://avatars1.githubusercontent.com/u/12463303?v=3&s=460", goals:{benchPress:275, currentBench:230, runPerMonth:50}},
     { name: "Denny", image: "https://avatars1.githubusercontent.com/u/2238156?v=3&s=40", goals:{benchPress:315, currentBench:275, runPerMonth:50}},
     { name: "Ariel", image: "https://avatars1.githubusercontent.com/u/16250211?v=3&u=69100363ede9ab6a3f86b2c48d23ac950bacbd8e&s=140", goals:{benchPress:215, currentBench:100, runPerMonth:50}},
-<<<<<<< HEAD
     { name: "Jason", image: "", goals:{benchPress:200, currentBench:165, runPerMonth:100}}
-=======
-    { name: "Jason", image: "", goals:{benchPress:220, currentBench:190, runPerMonth:100}}
->>>>>>> 5e757128ca629e5ee0ed4246f35090100951cd18
   ]
 }
