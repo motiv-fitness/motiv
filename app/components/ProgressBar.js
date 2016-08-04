d2babf160d30967acc30ca86ad133f60049b4d81
Ariel <arielcook101014@gmail.com>
2016-08-04 13:42:23 -0700

    [feature] add progressbar

---
+++ app/components/ProgressBar.js
@@ -0,0 +1,40 @@
+import React, {Component} from 'react';
+import {connect} from 'react-redux';
+
+var Line = require('rc-progress').Line;
+var SPACE = ".............";
+
+class ProgressBar extends Component {
+  renderProgressBar() {
+    return this.props.users.map((user, index) => {
+      return (
+        <div key={index}>
+          <Line percent={(user.goals.currentBench / user.goals.benchPress) * 100} strokeWidth={2} />
+          <div>
+            <strong>Name:</strong> {user.name} {SPACE}
+            <strong>Bench goal:</strong> {user.goals.benchPress} {SPACE}
+            <strong>Bench now:</strong> {user.goals.currentBench} {SPACE}
+            <strong>Line Progress:</strong> {(user.goals.currentBench / user.goals.benchPress) * 100}%
+          </div><br/>
+        </div>
+      )
+    })
+  }
+
+render() {
+    return (
+      <div>
+        <h2>Progress Bar</h2>
+        {this.renderProgressBar()}
+      </div>
+    )
+  }
+}
+
+function mapStateToProps(state) {
+  return {
+    users: state.goals
+  };
+}
+
+export default connect(mapStateToProps)(ProgressBar);
---
+++ app/reducers/goals.js
@@ -0,0 +1,8 @@
+export default function() {
+  return [
+    { name: "Justin", image: "https://avatars1.githubusercontent.com/u/12463303?v=3&s=460", goals:{benchPress:275, currentBench:230, runPerMonth:50}},
+    { name: "Denny", image: "https://avatars1.githubusercontent.com/u/2238156?v=3&s=40", goals:{benchPress:315, currentBench:275, runPerMonth:50}},
+    { name: "Ariel", image: "https://avatars1.githubusercontent.com/u/16250211?v=3&u=69100363ede9ab6a3f86b2c48d23ac950bacbd8e&s=140", goals:{benchPress:215, currentBench:100, runPerMonth:50}},
+    { name: "Jason", image: "", goals:{benchPress:200, currentBench:165, runPerMonth:100}}
+  ]
+}
