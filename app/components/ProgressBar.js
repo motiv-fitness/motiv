c605c390470476bd728b3392f6304f49e32ff2ce
Ariel <arielcook101014@gmail.com>
2016-08-04 10:10:02 -0700

    [feature] add progressbar

--- app/components/ProgressBar.js
+++ app/components/ProgressBar.js
@@ -1,7 +1,6 @@
 import React, {Component} from 'react';
 import {connect} from 'react-redux';
 
-<<<<<<< HEAD
 var Line = require('rc-progress').Line;
 var SPACE = ".............";
 
@@ -40,18 +39,3 @@ function mapStateToProps(state) {
 }
 
 export default connect(mapStateToProps)(ProgressBar);
-=======
-class ProgressBar extends Component {
-  renderProgressBar() {
-    return (?????)
-  }
-
-
-
-  render() {
-    return (
-      <div className='progress-bar'></div>
-    )
-  }
-}
->>>>>>> [Add folders] reducers and containers
