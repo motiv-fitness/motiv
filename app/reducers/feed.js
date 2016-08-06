8c40efbd99f2c28a5ed7dddacbc1b50ace799100
Justin Chen <justinpchen94@gmail.com>
2016-08-06 13:24:20 -0700

    [refactor] remove extra logs

--- app/reducers/feed.js
+++ app/reducers/feed.js
@@ -1,7 +1,6 @@
 export default function feed(state = {}, action) {
   switch (action.type) {
     case 'DISPLAY_FEED':
-      console.log("inside the display feed reducer *****", action)
       return Object.assign({}, state, {
         feedItems: action.payload
       });
