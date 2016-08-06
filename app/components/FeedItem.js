38d32536eb2f6f7baaa3e9e067d04c87fe555513
Justin Chen <justinpchen94@gmail.com>
2016-08-06 13:22:37 -0700

    [feature] display props instead of hardcoded text in feeditem

--- app/components/FeedItem.js
+++ app/components/FeedItem.js
@@ -4,15 +4,11 @@ const FeedItem = (props) => {
 
   return (
       <div>        
-        <h1> {props.data} </h1>
-
-        <h3> Justin Chen </h3>
-        <h4> 3:45 PM 08/05/2016 </h4>
-        <p> check out these status updates</p>
-
+        <h3> {props.name} </h3>
+        <h4> {props.time}</h4>
+        <p> {props.content}</p>
       </div>
   )
 }
-
 
 export default FeedItem;
