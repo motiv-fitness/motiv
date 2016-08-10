ab990ee33477b57f4015e4c1d2683dd3a017c65a
Justin Chen <justinpchen94@gmail.com>
2016-08-09 19:33:39 -0700

    [style] remove extra commented code

--- app/components/Header.js
+++ app/components/Header.js
@@ -52,30 +52,7 @@ class Header extends React.Component {
       </li>
     </ul>
       )
-    // const rightNav = this.state.token ? (
-    //   <ul className="list-inline">
-    //       <li>
-    //         <img className="avatar" src={this.state.user.picture || this.state.user.gravatar}/>
-    //         {' '}{this.state.user.name || this.state.user.email || this.state.user.id}{' '}
-    //       </li>
-    //       <li><Link to="/account">Edit Account</Link></li>
-    //       <li><Link to="/profile">View Profile</Link></li>
-    //       <li><a href="#" onClick={this.handleLogout.bind(this)}>Logout</a></li>
-    //   </ul>
-    // ) : (
-    //   <ul className="list-inline">
-    //       <li><Link to="/login">Log in</Link></li>
-    //       <li><Link to="/signup">Sign up</Link></li>
-    //   </ul>
-    // );
     return (
-      // <div className="container">
-      //   <ul className="list-inline">
-      //     <li><IndexLink to="/">Splash</IndexLink></li>
-      //     <li><Link to="/contact">Contact</Link></li>
-      //   </ul>
-      //   {rightNav}
-      // </div>
         <nav className="navbar navbar-inverse">
           <div className="container-fluid">
             <div className="navbar-header">
