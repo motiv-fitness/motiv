3287d352989d5fce7de4542b90c37af5f1c35237
Denny Jun <denny.6a756e@gmail.com>
2016-08-12 16:58:10 -0700

    [refactor] update table name

--- server/models/FavResource.js
+++ server/models/FavResource.js
@@ -2,7 +2,7 @@ var bookshelf = require('../config/bookshelf');
 var ModelBase = require('bookshelf-modelbase')(bookshelf);
 
 module.exports = (function() {
-  return bookshelf.model('Favresources', ModelBase.extend({
+  return bookshelf.model('Favresource', ModelBase.extend({
     tableName: 'favresources',
     user: function() {
       return this.belongsTo('User', 'user_id');
