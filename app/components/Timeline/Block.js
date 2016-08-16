bd3fa23182761f6d7b689a2c583f1c434dc09dd8
Denny Jun <denny.6a756e@gmail.com>
2016-08-15 20:12:57 -0700

    [feat] add additional progress data to timeline

--- app/components/Timeline/Block.js
+++ app/components/Timeline/Block.js
@@ -7,7 +7,7 @@ const Block = ({blockType, link, dateTime, weight, current, name, description, m
     ? undefined
     : blockType.className === 'cd-picture'
     ? (<img src={link} />)
-    : (<iframe width="400" height="240" src={link} frameBorder="0" allowFullScreen></iframe>);
+    : (<iframe width="100%" height="300" src={link} frameBorder="0" allowFullScreen></iframe>);
 
   const date = moment(dateTime).format('MM/DD/YYYY hh:mm A');
 
@@ -19,6 +19,28 @@ const Block = ({blockType, link, dateTime, weight, current, name, description, m
       <div className="cd-timeline-content">
         {media}
         <span className="cd-date">{date}</span>
+        <ul className="timeline-data-list list-group">
+          <li className="list-group-item">
+            Name:
+            <span className="badge">{name}</span>
+          </li>
+          <li className="list-group-item">
+            Type:
+            <span className="badge">{progressType}</span>
+          </li>
+          <li className="list-group-item">
+            Description:
+            <span className="badge">{description}</span>
+          </li>
+          <li className="list-group-item">
+            Weight:
+            <span className="badge">{weight}</span>
+          </li>
+          <li className="list-group-item">
+            Current progress:
+            <span className="badge">{current} {measurement}</span>
+          </li>
+        </ul>
       </div> 
     </div>
   );
