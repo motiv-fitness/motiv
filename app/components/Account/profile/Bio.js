3a1bd0eab1c2a973fc4be9b14df9ba74933f5d1a
Denny Jun <denny.6a756e@gmail.com>
2016-08-17 12:26:02 -0700

    [style] update profile style

--- app/components/Account/profile/Bio.js
+++ app/components/Account/profile/Bio.js
@@ -1,31 +1,27 @@
 import React from 'react';
 import Image from './Image';
 
-const Bio = ({email, name, gender, location, website, picture}) => {
+const Bio = ({email, name, location, website, picture}) => {
   const pictureDom = picture 
     ? (<Image src={picture} />)
     : undefined;
   const nameDom = name
-    ? (<div>name:<span>{name}</span></div>)
-    : undefined;
-  const genderDom = gender
-    ? (<div>gender:<span>{gender}</span></div>)
+    ? (<div><h3>{name}</h3></div>)
     : undefined;
   const emailDom = email
-    ? (<div>email:<span>{email}</span></div>)
+    ? (<div><span>{email}</span></div>)
     : undefined;
   const locationDom = location
-    ? (<div>location:<span>{location}</span></div>)
+    ? (<div><span>{location}</span></div>)
     : undefined;
   const websiteDom = website
-    ? (<div>website:<a href={website}>My Website</a></div>)
+    ? (<div><a href={website}>My Website</a></div>)
     : undefined;
 
   return (
-    <div>
+    <div className="bio-div">
       {pictureDom}
       {nameDom}
-      {genderDom}
       {emailDom}
       {locationDom}
       {websiteDom}
