d701a45eba80c24b45a4cc7aefa2edacafd80535
Ariel <arielcook101014@gmail.com>
2016-08-20 16:18:30 -0700

    [feat] add goals and goal functionality with CRUD operations

--- server/controllers/private/goal.js
+++ server/controllers/private/goal.js
@@ -2,7 +2,7 @@ var ControllerPrototype = require('../controller.prototype');
 var Goal = require('../../models/Goal');
 var User = require('../../models/User');
 var ProgressName = require('../../models/ProgressName');
-var ProgressLogs = require('../../models/ProgressLog');
+var ProgressLog = require('../../models/ProgressLog');
 var _ = require('lodash');
 
 module.exports = (function() {
@@ -14,21 +14,18 @@ var router = controller.router;
 
   // POST
   router.post('/', function(req, res) {
-    console.log("------inside post req.body.goal", req.body.goal)
     User.findOne({
       id: req.user.id
     }).then(function(user) {
-      console.log("======req.body.goal.measurement", req.body.goal.measurement)
       return user.goals().create({
-        target: req.body.goal.target,
-        measurement: req.body.goal.measurementValue
+        target: req.body.goalAdd.target,
+        measurement: req.body.goalAdd.measurementValue
       });
     }).then(function(goal) {
-      console.log("=========== req.body.goal", req.body.goal)
       return ProgressName.create({
-        type: req.body.goal.typeValue,
-        name: req.body.goal.name,
-        description: req.body.goal.description,
+        type: req.body.goalAdd.typeValue,
+        name: req.body.goalAdd.name,
+        description: req.body.goalAdd.description,
         goal_id: goal.id
       });
     }).then(function(progressName) {
@@ -39,40 +36,81 @@ var router = controller.router;
     });
   });
 
-  //GET THE GOOD VERSION
-  console.log('========about to run get from back end')
-  router.get('/', function(req, res) {
+  router.put('/', function(req, res) {
+    Goal.findOne({
+      id: req.body.goalUpdate.goalId,
+    })
+    .then(function(data) {
+      Goal.update({target: req.body.goalUpdate.target,
+        measurement: req.body.goalUpdate.measurementValue, }, {id: req.body.goalUpdate.goalId})
+    })
+    .then(function(data) {
+      ProgressName.findOne({
+        goal_id: req.body.goalUpdate.goalId
+      })
+      .then(function(progName) {
+        progName.save({name: req.body.goalUpdate.name,
+              description: req.body.goalUpdate.description,
+              type: req.body.goalUpdate.typeValue}, {goal_id: req.body.goalUpdate.goalId}) //id is the primary key
+        })
+      })
+    })
+
+  router.delete('/', function(req, res) {
+    Goal.findOne({
+      id: req.body.goalDelete.goalId
+    })
+    .then(function(goal) {
+      goal.destroy()
+    })
+    .then(function(goal) {
+      ProgressName.findOne(function(data) {
+        goal_id: req.body.goalDelete.goalId
+      })
+      .then(function(progName) {
+        progName.destroy()
+        })
+    })
+  })
+
+ router.get('/', function(req, res) {
     Goal.fetchAll({
       user_id: req.user.id,
       withRelated:['progressName']
     }).then(function(goals) {
-      console.log("---------backend: goals.models", goals.models)
       var data = _.map(goals.models, function(goal) {
-        return {
-          name: goal.relations.progressName.attributes.name,
-          target: goal.attributes.target,
-          measurement: goal.attributes.measurement
-        };
+        if(goal.relations.progressName.attributes.progressLog_id) {
+          return ProgressLog.findOne({
+            id: goal.relations.progressName.attributes.progressLog_id
+          }).then(function(progressLog) {
+            return {
+              target: goal.attributes.target,
+              measurement: goal.attributes.measurement,
+              name: goal.relations.progressName.attributes.name,
+              description: goal.relations.progressName.attributes.description,
+              type: goal.relations.progressName.attributes.type,
+              current: progressLog.attributes.current,
+              goal_id: goal.id
+            };
+          });
+        } else {
+          return {
+            target: goal.attributes.target,
+            measurement: goal.attributes.measurement,
+            name: goal.relations.progressName.attributes.name,
+            description: goal.relations.progressName.attributes.description,
+            type: goal.relations.progressName.attributes.type,
+            current: 0,
+            goal_id: goal.id
+          };
+        }
       });
-      console.log("-------------backend data", data)
-      res.json(data);
-    })
-  });
-
-
-// GET ALL DATA FROM PROGRESS NAME
-  // router.get('/', function(req, res) {
-  //   ProgressName.forge()
-  //   .fetch()
-  //   .then(function (collection) {
-  //     console.log("==================collection", collection)
-  //     res.json({error: false, data: collection.toJSON()});
-  //   })
-  //   .catch(function (err) {
-  //     res.status(500).json({error: true, data: {message: err.message}});
-  //   });
-  // })
-
+      return Promise.all(data)
+      .then(function(results) {
+        res.json(results);
+      });
+    });
+ });
 
 return controller;
 
