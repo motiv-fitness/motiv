0f63d9e0f1dd4b6d66703c0390724d190ab467c1
Denny Jun <denny.6a756e@gmail.com>
2016-09-03 23:34:04 -0700

    [bug] fix minimized height size issue

--- app/components/Splash.js
+++ app/components/Splash.js
@@ -13,31 +13,36 @@ class Splash extends React.Component {
               <source src="../../assets/stretching.mp4" type="video/mp4"/>
           </video>
         </div>
-        <Parallax strength={400} className="parallax-row" bgImage="https://static.pexels.com/photos/17840/pexels-photo.jpg">
+        <Parallax strength={400} className="parallax-row"
+          bgImage="https://static.pexels.com/photos/17840/pexels-photo.jpg">
           <ParallaxHover width='500' height='200'>
             <img ref="image" className="landing-page-parallax-overlay" alt=" " />
             <h1 ref="text">STRENGTH</h1>
           </ParallaxHover>
         </Parallax>
-        <Parallax strength={400} className="parallax-row" bgImage="https://aos.iacpublishinglabs.com/question/aq/700px-394px/old-join-la-fitness_4db9384490d8bf5f.jpg?domain=cx.aos.ask.com">
+        <Parallax strength={400} className="parallax-row"
+          bgImage="https://aos.iacpublishinglabs.com/question/aq/700px-394px/old-join-la-fitness_4db9384490d8bf5f.jpg?domain=cx.aos.ask.com">
           <ParallaxHover width='500' height='200'>
             <img ref="image" className="landing-page-parallax-overlay" alt=" " />
             <h1 ref="text">CONFIDENCE</h1>
           </ParallaxHover>
         </Parallax> 
-        <Parallax strength={400} className="parallax-row" bgImage="http://images.askmen.com/1080x540/2016/01/06-040026-how_to_track_your_fitness_progress.jpg">
+        <Parallax strength={400} className="parallax-row"
+          bgImage="http://images.askmen.com/1080x540/2016/01/06-040026-how_to_track_your_fitness_progress.jpg">
           <ParallaxHover width='500' height='200'>
             <img ref="image" className="landing-page-parallax-overlay" alt=" " />
             <h1 ref="text">ENDURANCE</h1>
           </ParallaxHover>
         </Parallax>
-        <Parallax strength={400} className="parallax-row" bgImage="http://www.coast-fitness.com/wp-content/uploads/2013/09/cf-page-headers-group-fitness-classes.png">
+        <Parallax strength={400} className="parallax-row"
+          bgImage="http://www.coast-fitness.com/wp-content/uploads/2013/09/cf-page-headers-group-fitness-classes.png">
           <ParallaxHover width='500' height='200'>
             <img ref="image" className="landing-page-parallax-overlay" alt=" " />
             <h1 ref="text">HEALTH</h1>
           </ParallaxHover>
         </Parallax>
-        <Parallax strength={400} className="parallax-row" bgImage="https://assets.entrepreneur.com/content/3x2/1300/20160201225029-weights-fitness-workout-work.jpeg">
+        <Parallax strength={400} className="parallax-row"
+            bgImage="https://assets.entrepreneur.com/content/3x2/1300/20160201225029-weights-fitness-workout-work.jpeg">
           <ParallaxHover width='500' height='200'>
             <img ref="image" className="landing-page-parallax-overlay" alt=" " />
             <h1 ref="text">WHAT'S YOUR MOTIVATION?</h1>
